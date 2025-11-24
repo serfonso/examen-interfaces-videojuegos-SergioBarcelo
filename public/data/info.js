480a625eb52ed35df31d146cd3e455cdf489e0d4
sergio <serfonso2005@gmail.com>
2025-11-24 17:08:15 +0100

    primer commit sin service

---
+++ public/data/info.js
@@ -0,0 +1,32 @@
+export const juegos = [
+    // PC
+    { id: 1, titulo: "Sky Warriors", plataforma: "PC" },
+    { id: 2, titulo: "Code Breaker", plataforma: "PC" },
+    { id: 3, titulo: "Galactic Tactics", plataforma: "PC" },
+    // PlayStation
+    { id: 4, titulo: "Dungeon Quest", plataforma: "PlayStation" },
+    { id: 5, titulo: "Storm Racers", plataforma: "PlayStation" },
+    { id: 6, titulo: "Legends of Avalon", plataforma: "PlayStation" },
+    // Xbox
+    { id: 7, titulo: "Racing Thunder",plataforma: "Xbox" },
+    { id: 8, titulo: "Iron Clash",plataforma: "Xbox" },
+    { id: 9, titulo: "Shadow Ops",plataforma: "Xbox" },
+    // Nintendo Switch
+    { id: 10, titulo: "Pixel Farm", plataforma: "Nintendo Switch" },
+    { id: 11, titulo: "Island Builder", plataforma: "Nintendo Switch" },
+    { id: 12, titulo: "Jump Heroes", plataforma: "Nintendo Switch" }
+    ];
+    export const valoraciones = [
+    { id: 1, puntuacion: 8.5 },
+    { id: 2, puntuacion: 7.9 },
+    { id: 3, puntuacion: 9.1 },
+    { id: 4, puntuacion: 9.2 },
+    { id: 5, puntuacion: 8.0 },
+    { id: 6, puntuacion: 8.7 },
+    { id: 7, puntuacion: 7.8 },
+    { id: 8, puntuacion: 8.3 },
+    { id: 9, puntuacion: 7.5 },
+    { id: 10, puntuacion: 8.0 },
+    { id: 11, puntuacion: 8.4 },
+    { id: 12, puntuacion: 7.6 }
+    ];
