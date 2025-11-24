85d5ff64bcbf4eba3b6de068c662ea55c78f216e
sergio <serfonso2005@gmail.com>
2025-11-24 17:18:11 +0100

    commit con el servicio getJuegoService

--- src/services/getJuegoService.js
+++ src/services/getJuegoService.js
@@ -4,6 +4,12 @@ export function getJuego(id) {
     return Promise((resolve, reject) => {
         setTimeout(() => {
             const juego = juegos.find( j => j.id === id)
+
+            if (juego) {
+                resolve (juego)
+            } else {
+                reject (`El juego con id ${id} no existe`)
+            }
         }, 800);
     })
 }
