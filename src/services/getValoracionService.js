aa50abadffd6d793db78ac970611394973f15c92
sergio <serfonso2005@gmail.com>
2025-11-24 17:22:14 +0100

    commit con el servicio getValoracionService

---
+++ src/services/getValoracionService.js
@@ -0,0 +1,15 @@
+import { valoraciones } from "../../public/data/info";
+
+export function getValoracion(id) {
+    return Promise((resolve, reject) => {
+        setTimeout(() => {
+            const valoracion = valoraciones.find( v => v.id === id)?.puntuacion
+
+            if (valoracion) {
+                resolve (valoracion)
+            } else {
+                reject (`la valoracion del juego con id ${id} no existe`)
+            }
+        }, 800);
+    })
+}
